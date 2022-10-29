6bbcfbe381d6e267fb468e90773ffe1975b7c758
Arturo <atercerosb@univalle.edu>
2022-10-29 13:22:24 -0400

    login and regiser YUP

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
-import './styles/Login.css';
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import React from "react";
+import "./styles/Login.css";
 export interface LoginInterface {}
 
-const Login : React.FC<LoginInterface> = () => {
-	return (
-		<div className='login'>
-			Login
-		</div >
-	)
+interface FormValues {
+  email: string;
+  password: string;
+}
+const Login: React.FC<LoginInterface> = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email").required("Required"),
+      password: Yup.string().required("No password provided."),
+    }),
+  });
+
+  return (
+    <div className="login">
+      <h3>Jobizz</h3>
+      <p>Welcame Back 👋</p>
+      <p>Let's log in, Apply to jobs!</p>
+
+      <form>
+        <input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <button type="submit">Log in</button>
+      </form>
+      <span>Forgot Password? </span>
+    </div>
+  );
 };
 
 export default Login;
--- src/page/Register/Register.tsx
+++ src/page/Register/Register.tsx
@@ -1,9 +1,109 @@
-import React from 'react';
-import './styles/Register.css';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import "./styles/Register.css";
 export interface RegisterInterface {}
 
-const Register : React.FC<RegisterInterface> = () => {
-	return <div className='register'>Register</div >;
+interface FormValues {
+  name: string;
+  email: string;
+  password1: string;
+  password2: string;
+}
+
+const Register: React.FC<RegisterInterface> = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password1: "",
+      password2: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email").required("Required"),
+      password1: Yup.string().required("No password provided."),
+      password2: Yup.string()
+        .oneOf([Yup.ref("password1"), null], "Passwords must match")
+        .required("Required"),
+    }),
+  });
+
+  return (
+    <div className="register">
+      <h3>Jobizz</h3>
+      <p>Registration 👍</p>
+      <p>Let's Register, Apply to jobs!</p>
+
+      <form noValidate onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First name"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.firstName && formik.errors.firstName && (
+          <span>{formik.errors.firstName}</span>
+        )}
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last name"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.lastName && formik.errors.lastName && (
+          <span>{formik.errors.lastName}</span>
+        )}
+        <input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.email && formik.errors.email && (
+          <span>{formik.errors.email}</span>
+        )}
+        <input
+          type="password"
+          name="password1"
+          placeholder="Password"
+          value={formik.values.password1}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.password1 && formik.errors.password1 && (
+          <span>{formik.errors.password1}</span>
+        )}
+        <input
+          type="password"
+          name="password2"
+          placeholder="Repet Password"
+          value={formik.values.password2}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.password2 && formik.errors.password2 && (
+          <span>{formik.errors.password2}</span>
+        )}
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
