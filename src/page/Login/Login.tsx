8c97b44f35c0d60937a8438db9ab72e8b1b41f20
Andres Parra <andeveling@gmail.com>
2022-10-29 23:26:57 -0500

    update/UI-MUI LOGIN & REGISTER

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,66 +1,80 @@
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import React from "react";
-import "./styles/Login.css";
+import { Button } from '@mui/material'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import { useFormik } from 'formik'
+import React from 'react'
+import * as Yup from 'yup'
 export interface LoginInterface {}
 
 interface FormValues {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 const Login: React.FC<LoginInterface> = () => {
   const formik = useFormik({
     initialValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
     onSubmit: (values) => {
-      console.log(values);
+      console.log(values)
     },
     validationSchema: Yup.object({
-      email: Yup.string().email("Invalid email").required("Required"),
-      password: Yup.string().required("No password provided."),
+      email: Yup.string().email('Invalid email').required('Required'),
+      password: Yup.string().required('No password provided.'),
     }),
-  });
+  })
 
   return (
-    <div className="login">
-      <div className="hero">
-        <h3>Jobizz</h3>
-        <p>Welcame Back 👋</p>
-        <p>Let's log in, Apply to jobs!</p>
-      </div>
+    <Container component='main' maxWidth='xs'>
+      <Box component='form' onSubmit={formik.handleSubmit} sx={{ mt: 2 }}>
+        <Grid container spacing={2} justifyContent='center' textAlign='center'>
+          <Grid item xs={12}>
+            <Typography variant='h5'>Jobizz Login</Typography>
+            <Typography>Welcame Back 👋</Typography>
+            <Typography variant='body2'>Let's log in, Apply to jobs!</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              id='email'
+              name='email'
+              label='email'
+              type='email'
+              placeholder='usuario o mail'
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </Grid>
 
-      <form className="login-form" noValidate onSubmit={formik.handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="E-mail"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <span>{formik.errors.email}</span>
-        )}
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.password && formik.errors.password && (
-          <span>{formik.errors.password}</span>
-        )}
-        <button type="submit">Log in</button>
-      </form>
-      <div className="button-section">
-        <span>Forgot Password? </span>
-      </div>
-    </div>
-  );
-};
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              id='password'
+              name='password'
+              label='password'
+              type='password'
+              placeholder='password'
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button size='large' fullWidth type='submit' variant='contained'>
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  )
+}
 
-export default Login;
+export default Login
